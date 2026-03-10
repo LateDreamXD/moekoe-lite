14fd688f9a3e5d239eb29eca940705b534375789
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-03-10 15:02:23 +0800

    feat(sidecar): add api server to sidecar

--- bin/api-build.ts
+++ bin/api-build.ts
@@ -1,23 +1,48 @@
 import { execSync } from 'child_process';
 import { resolve } from 'path';
+import { cpSync, existsSync } from 'fs';
 
-const timer = Date.now();
 const apiPath = resolve(process.cwd(), 'core/api');
 
-const os = (() => {switch (process.platform) {
-	case 'win32':
-		return 'win';
-	case 'linux':
-		if (process.arch === 'arm64')
-			return 'linux-arm64';
-		return 'linux';
-	case 'darwin':
-		return 'macos';
-	default:
-		throw new Error(`unsupported platform: '${process.platform}'`);
-}})();
+const os = (() => {
+	switch (process.platform) {
+		case 'win32':
+			return 'win';
+		case 'linux':
+			if (process.arch === 'arm64')
+				return 'linux-arm64';
+			return 'linux';
+		case 'darwin':
+			return 'macos';
+		default:
+			throw new Error(`unsupported platform: '${process.platform}'`);
+	}
+})();
 
-console.log(`building api server for ${os}...`);
+const ext = process.platform === 'win32' ? '.exe' : '';
+const binPath = resolve(apiPath, `bin/app_${os}${ext}`);
+const rustInfo = execSync('rustc -vV').toString();
+const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
+if (!targetTriple) {
+	console.error('Failed to determine platform target triple');
+	process.exit(1);
+}
 
-execSync(`npm run pkg${os}`, { cwd: apiPath });
-console.log(`api server build completed in ${Date.now() - timer}ms`);
+if (existsSync(binPath)) {
+	console.log('found last built binary, skip building');
+	cpSync(
+		binPath,
+		`./src-tauri/bin/api-${targetTriple}${ext}`
+	);
+} else {
+	console.log(`building api server for ${os}...`);
+	const timer = Date.now();
+
+	execSync(`npm run pkg${os}`, { cwd: apiPath });
+	cpSync(
+		binPath,
+		`./src-tauri/bin/api-${targetTriple}${ext}`
+	);
+
+	console.log(`api server build completed in ${Date.now() - timer}ms`);
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,56 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::sync::{Arc, Mutex};
+use tauri::{Manager, RunEvent};
+use tauri_plugin_shell::{process::CommandChild, ShellExt};
+
+fn start_api_server(app_handle: tauri::AppHandle) -> Result<(), String> {
+    if let Some(state) = app_handle.try_state::<Arc<Mutex<Option<CommandChild>>>>() {
+        let child_process = state.lock().unwrap();
+        if child_process.is_some() {
+            println!("api server is already running");
+            return Ok(());
+        }
+    }
+    let api_cmd = app_handle
+        .shell()
+        .sidecar("api")
+        .unwrap()
+        .arg("--platform=lite")
+        .arg("--port=6521");
+    let (_rv, child) = api_cmd.spawn().map_err(|err| err.to_string())?;
+
+    if let Some(state) = app_handle.try_state::<Arc<Mutex<Option<CommandChild>>>>() {
+        *state.lock().unwrap() = Some(child);
+    } else {
+        return Err("failed to access api server state".to_string());
+    }
+
+    Ok(())
+}
+
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            app.manage(Arc::new(Mutex::new(None::<CommandChild>)));
+            let app_handle = app.handle().clone();
+            println!("starting api server");
+            start_api_server(app_handle).map_err(|err| err.to_string())?;
+            Ok(())
+        })
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|app_handle, ev| match ev {
+            RunEvent::ExitRequested { .. } => {
+                if let Some(state) = app_handle.try_state::<Arc<Mutex<Option<CommandChild>>>>() {
+                    let mut child_process = state.lock().unwrap();
+                    if let Some(child) = child_process.take() {
+                        let _ = child.kill().unwrap();
+                    }
+                }
+            }
+            _ => {}
+        })
 }
