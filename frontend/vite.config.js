8b1bd1cb8fd9bd9ed20bc9b91ac5ab8452fe63ed
latedream <xiaozhisans@qq.com>
2026-06-27 18:50:28 +0800

    chore: remove useless lines

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import fakePwaPlugin from './plugin/fake-pwa';
 
-import { resolve } from 'path';
-
 import { version } from './src/package.json';
 
 const cwd = process.cwd();
 
 export default defineConfig({
 	base: '',
-	build: {
-		outDir: resolve(cwd, 'dist')
-	},
 	define: {
 		__VERSION__: JSON.stringify(version)
 	},
