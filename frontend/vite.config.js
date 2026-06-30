ce4f4bc82c1de23eacf6e56e39795375d27ccfc6
latedream <xiaozhisans@qq.com>
2026-07-01 00:28:48 +0800

    revert: chore: remove useless lines
    This reverts commit 8b1bd1cb8fd9bd9ed20bc9b91ac5ab8452fe63ed.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,17 @@ import vue from '@vitejs/plugin-vue';
 import liteCore from './plugin/lite-core';
 import fakePwaPlugin from './plugin/fake-pwa';
 
+import { resolve } from 'path';
+
+import { version } from './src/package.json';
+
 const cwd = process.cwd();
 
 export default defineConfig({
 	base: '',
+	build: {
+		outDir: resolve(cwd, 'dist')
+	},
 	plugins: [vue(), liteCore(), fakePwaPlugin()],
 
 	resolve: {
