af93d13aa2845fe1aee74762dd8f592a386b255d
latedream <xiaozhisans@qq.com>
2026-07-10 22:14:18 +0800

    build: replace to wails v3

--- main.go
+++ main.go
@@ -3,37 +3,98 @@ package main
 import (
 	"embed"
 
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"log"
+	"time"
+
+	"github.com/wailsapp/wails/v3/pkg/application"
 )
+
+type App struct {
+	ctx        *application.App
+	mainWindow *application.WebviewWindow
+	mvWindow   *application.WebviewWindow
+}
+
+// Wails uses Go's `embed` package to embed the frontend files into the binary.
+// Any files in the frontend/dist folder will be embedded into the binary and
+// made available to the frontend.
+// See https://pkg.go.dev/embed for more information.
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
-func main() {
-	// Create an instance of the app structure
-	app := NewApp()
+func init() {
+	// Register a custom event whose associated data type is string.
+	// This is not required, but the binding generator will pick up registered events
+	// and provide a strongly typed JS/TS API for them.
+	application.RegisterEvent[string]("time")
+}
 
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "Moekoe Music Lite",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets: assets,
+// main function serves as the application's entry point. It initializes the application, creates a window,
+// and starts a goroutine that emits a time-based event every second. It subsequently runs the application and
+// logs any error that might occur.
+func main() {
+	app := &App{}
+
+	// Create a new Wails application by providing the necessary options.
+	// Variables 'Name' and 'Description' are for application metadata.
+	// 'Assets' configures the asset server with the 'FS' variable pointing to the frontend files.
+	// 'Bind' is a list of Go struct instances. The frontend has access to the methods of these instances.
+	// 'Mac' options tailor the application when running an macOS.
+	app.ctx = application.New(application.Options{
+		Name:        "moekoemusic-lite",
+		Description: "萌音极速版",
+		Services:    []application.Service{},
+		Assets: application.AssetOptions{
+			Handler: application.AssetFileServerFS(assets),
 		},
-		OnStartup: app.startup,
-		Bind: []interface{}{
-			app,
+		Mac: application.MacOptions{
+			ApplicationShouldTerminateAfterLastWindowClosed: true,
 		},
-		SingleInstanceLock: &options.SingleInstanceLock{
-			UniqueId:               "6d6f656b-6f65-8c69-b465-2d62792d6c61",
-			OnSecondInstanceLaunch: app.onSecondInstanceLaunch,
+		SingleInstance: &application.SingleInstanceOptions{
+			UniqueID: "cn.latedream.moekoemusic-lite",
+			OnSecondInstanceLaunch: func(data application.SecondInstanceData) {
+				app.mainWindow.UnMinimise()
+				app.mainWindow.Focus()
+			},
 		},
 	})
 
+	// Create a new window with the necessary options.
+	// 'Title' is the title of the window.
+	// 'Mac' options tailor the window when running on macOS.
+	// 'BackgroundColour' is the background colour of the window.
+	// 'URL' is the URL that will be loaded into the webview.
+	app.mainWindow = app.ctx.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title: "MoeKoeLite 萌音极速版",
+		// Window sized to the golden ratio (1000 / 618 ≈ 1.618).
+		Width:  1000,
+		Height: 618,
+		Mac: application.MacWindow{
+			InvisibleTitleBarHeight: 50,
+			Backdrop:                application.MacBackdropTranslucent,
+			TitleBar: application.MacTitleBar{
+				AppearsTransparent: true,
+			},
+		},
+		URL: "/",
+	})
+
+	// Create a goroutine that emits an event containing the current time every second.
+	// The frontend can listen to this event and update the UI accordingly.
+	go func() {
+		for {
+			now := time.Now().Format(time.RFC1123)
+			app.ctx.Event.Emit("time", now)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	// Run the application. This blocks until the application has been exited.
+	err := app.ctx.Run()
+
+	// If an error occurred while running the application, log it and exit.
 	if err != nil {
-		println("Error:", err.Error())
+		log.Fatal(err)
 	}
 }
